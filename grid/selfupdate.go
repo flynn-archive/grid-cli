87ce2d2647bcc6c53de8d7a9c2843fefe95b0850
Jeff Lindsay <progrium@gmail.com>
2014-01-09 13:29:17 -0600

    fleshing out selfupdate code and syncing travis output to work with it

--- grid/selfupdate.go
+++ grid/selfupdate.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"runtime"
+
+	"bitbucket.org/kardianos/osext"
 )
+
+// TODO: checksum downloads
 
 var cmdSelfUpdate = &Command{
 	Run:   runSelfUpdate,
@@ -17,5 +25,37 @@ func runSelfUpdate(cmd *Command, args []string) {
 		cmd.printUsage()
 		os.Exit(2)
 	}
-	fmt.Println("selfupdate!")
+	resp, err := http.Get("https://s3.amazonaws.com/progrium-flynn/flynn-grid/dev/version")
+	assert(err)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	assert(err)
+	version := string(body)
+	if version == "dev" {
+		updateSelf()
+		return
+	}
+	if version == Version {
+		fmt.Println("Already up to date.")
+	}
+	updateSelf()
+}
+
+func updateSelf() {
+	resp, err := http.Get("https://s3.amazonaws.com/progrium-flynn/flynn-grid/dev/grid-cli_" + runtime.GOOS + "_" + runtime.GOARCH + ".tgz")
+	assert(err)
+	defer resp.Body.Close()
+	selfpath, err := osext.Executable()
+	assert(err)
+	info, err := os.Stat(selfpath)
+	assert(err)
+	f, err := ioutil.TempFile("", "grid-update")
+	assert(err)
+	_, err = io.Copy(f, resp.Body)
+	assert(err)
+	f.Close()
+	assert(f.Chmod(info.Mode().Perm()))
+	assert(os.Remove(selfpath))
+	assert(os.Rename(f.Name(), selfpath))
+	fmt.Println("Updated.")
 }
