ac5ffe5d23f5041c4b40f22a05a0c835cf05fb62
Jeff Lindsay <progrium@gmail.com>
2014-01-31 17:58:56 -0600

    updating to use the new go-flynn api
    Signed-off-by: Jeff Lindsay <progrium@gmail.com> (github: progrium)

--- grid/schedule.go
+++ grid/schedule.go
@@ -5,12 +5,10 @@ import (
 	"os"
 	"strings"
 
+	"github.com/flynn/flynn-host/types"
 	"github.com/flynn/go-discoverd"
 	"github.com/flynn/go-dockerclient"
-	hosts "github.com/flynn/lorne/client"
-	"github.com/flynn/sampi/client"
-	"github.com/flynn/sampi/types"
-	"github.com/nu7hatch/gouuid"
+	"github.com/flynn/go-flynn/cluster"
 )
 
 var cmdSchedule = &Command{
@@ -27,16 +25,18 @@ func runSchedule(cmd *Command, args []string) {
 	}
 	err := discoverd.Connect(getTarget() + ":55002") // TODO: fix this
 	assert(err)
-	sched, err := client.New()
+	client, err := cluster.NewClient()
 	assert(err)
 
-	hosts, err := discoverd.Services("flynn-lorne", discoverd.DefaultTimeout)
+	hosts, err := client.ListHosts()
 	assert(err)
-	hostid := hosts[0].Attrs["id"]
+	var hostid string
+	for id, _ := range hosts {
+		hostid = id
+		break
+	}
 
-	id, err := uuid.NewV4()
-	jobid := id.String()
-	assert(err)
+	jobid := cluster.RandomJobID(args[0])
 	config := docker.Config{
 		Image: args[0],
 		//Cmd:          []string{"start", "web"},
@@ -48,29 +48,40 @@ func runSchedule(cmd *Command, args []string) {
 		StdinOnce:    false,
 	}
 
-	schedReq := &sampi.ScheduleReq{
+	jobReq := &host.AddJobsReq{
 		Incremental: true,
-		HostJobs: map[string][]*sampi.Job{
+		HostJobs: map[string][]*host.Job{
 			hostid: {{ID: jobid, Config: &config, TCPPorts: 1}}},
 	}
-	_, err = sched.Schedule(schedReq)
+	_, err = client.AddJobs(jobReq)
 	assert(err)
 
-	if port := getPort(hostid, jobid); port != "" {
-		fmt.Println(jobid + " created, listening at " + hosts[0].Host + ":" + port)
+	if addr := getAddr(client, hostid, jobid); addr != "" {
+		fmt.Println(jobid + " created, listening at " + addr)
 	} else {
 		fmt.Println("Service was not scheduled")
 		os.Exit(1)
 	}
 }
 
-func getPort(hostid, jobid string) string {
-	host, err := hosts.New(hostid)
+func getAddr(client *cluster.Client, hostid, jobid string) string {
+	services, err := discoverd.Services("flynn-host", discoverd.DefaultTimeout)
+	var service *discoverd.Service
+	for _, s := range services {
+		if s.Attrs["id"] == hostid {
+			service = s
+			break
+		}
+	}
+	if service == nil {
+		return ""
+	}
+	host, err := client.ConnectHost(hostid)
 	assert(err)
 	job, err := host.GetJob(jobid)
 	assert(err)
 	for portspec := range job.Job.Config.ExposedPorts {
-		return strings.Split(portspec, "/")[0]
+		return service.Host + ":" + strings.Split(portspec, "/")[0]
 	}
 	return ""
 }
